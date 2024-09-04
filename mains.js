20163ecafecf4674910eed67846866a84a33891f
Derisbel <derisbelr@gmail.com>
2024-09-03 21:30:22 -0400

    Add files via upload
    sueldp

--- mains.js
+++ mains.js
@@ -1,10 +1,11 @@
-const operacion = prompt(" Para el promedio 1 para el sueldo 2")
+const operacion = prompt(" Para el promedio 1 para el sueldo 2 ")
 
 console.log(operacion)
 switch (operacion) {
     case "1":
-        promedio()
+        primedio()
         break;
+        case "2":
         tuSueldo()
         break;
     default:
@@ -22,24 +23,25 @@ function tuSueldo (){
 }
 
 function promedio () {
-//Declarar la variable 
-const nu1 = prompt("Ingrese el numero 1")
-const nu2 = prompt("Ingrese el primer 2")
-const nu3 = prompt("Ingrese el primer 3")
-
-//covertir a entero
-const nu1Int = Number(nu1)
-const nu2Int = Number(nu2)
-const nu3Int = Number(nu3)
-
-//sumar los numero
-const resultado = nu1Int + nu2Int + nu3Int
-
-//calcular el promedio
-const promedio = resultado / 3
-// mostrar el resultado
-
-alert(" Promedio de los tres numeros es:" + promedio )
-}
-
-
+    //Declarar la variable 
+    const nu1 = prompt("Ingrese el numero 1")
+    const nu2 = prompt("Ingrese el primer 2")
+    const nu3 = prompt("Ingrese el primer 3")
+    
+    //covertir a entero
+    const nu1Int = Number(nu1)
+    const nu2Int = Number(nu2)
+    const nu3Int = Number(nu3)
+    
+    //sumar los numero
+    const resultado = nu1Int + nu2Int + nu3Int
+    
+    //calcular el promedio
+    const promedio = resultado / 3
+    // mostrar el resultado
+    
+    alert(" Promedio de los tres numeros es:" + promedio )
+    }
+    
+    
+    
