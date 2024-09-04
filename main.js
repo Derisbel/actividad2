2a98d6daefe59e0cd15a1044e5529fa3300208ad
Derisbel <derisbelr@gmail.com>
2024-09-03 20:24:59 -0400

    tarea
    Diseñemos un Diagrama de Flujo- El sueldo

---
+++ main.js
@@ -0,0 +1,18 @@
+//Declarar la variable 
+const nu1=prompt("Ingrese el numero 1")
+const nu2=prompt("Ingrese el primer 2")
+const nu3=prompt("Ingrese el primer 3")
+
+//covertir a entero
+const nu1Int = Number(nu1)
+const nu2Int = Number(nu2)
+const nu3Int = Number(nu3)
+
+//sumar los numero
+const resultado = nu1Int + nu2Int + nu3Int
+
+//calcular el promedio
+const promedio = resultado / 3
+// mostrar el resultado
+
+alert("Promedio de los tres numeros es:" + promedio)
